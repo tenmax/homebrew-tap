345137bc72669880a92b789944f48b815b6bb435
Study <ph.study@gmail.com>
2016-04-19 20:13:44 +0800

    update azurekit sha256

--- azurekit.rb
+++ azurekit.rb
@@ -2,12 +2,13 @@ class Azurekit < Formula
   desc "CLI toolkit to interact with microsoft azure blob storage."
   homepage "https://github.com/tenmax/azurekit"
   url "https://github.com/tenmax/azurekit/releases/download/v0.2.0/azurekit-0.2.0.zip"
-  sha256 "f4c1fe80fb203df4f53d56ad6876ceee55fc15c2ce64c447db3f84a5fb8e047e"
+  sha256 "2571d8bc86c70807cb5da7a18338ea6169447b420cf9e6c790b1503cc876f6a9"
 
   bottle :unneeded
 
   def install
     libexec.install %w[bin lib]
+    bin.install_symlink libexec+"bin/azurecat"
     bin.install_symlink libexec+"bin/azuresas"
     bin.install_symlink libexec+"bin/azuresink"
   end
