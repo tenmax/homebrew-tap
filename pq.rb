a7d8108e6fe2f6357dfbfc6ebd8c43cec477a7d4
Study <ph.study@gmail.com>
2016-08-12 16:04:34 +0800

    add pq

---
+++ pq.rb
@@ -0,0 +1,13 @@
+class Pq < Formula
+  desc "A tool to transform unstructured text to csv by regular expression."
+  homepage "https://github.com/tenmax/pq"
+  url "https://github.com/tenmax/pq/releases/download/v1.0.0/pq-1.0.0.zip"
+  sha256 "c969106520d34073498f172325e3ef573756375afdb844fc9d5fd81d0fb968b7"
+
+  bottle :unneeded
+
+  def install
+    libexec.install %w[bin lib]
+    bin.install_symlink libexec+"bin/pq"
+  end
+end
