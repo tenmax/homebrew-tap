5652410419adbed9297f0e2ed5a610704c783ecb
Study <ph.study@gmail.com>
2017-06-16 12:40:38 +0800

    update sha256

--- dblk-protokit.rb
+++ dblk-protokit.rb
@@ -2,7 +2,7 @@ class DblkProtokit < Formula
   desc "DoubleClick ADX bidrequest and bidrespone json2proto and proto2json utils."
   homepage "https://github.com/tenmax/dblk-protokit"
   url "https://github.com/tenmax/dblk-protokit/releases/download/v1.0.0/dblk-proto2json-1.0.0.zip"
-  sha256 "aa2d902d0c6f7bdde18b2d096775579f38fc66fd6f1d70b88aeaa447b2e543b4"
+  sha256 "85cfc7f5b98e756a8aba045a68e968a13955e4f65b684d85b2f544d2a0c90040"
 
   bottle :unneeded
 
