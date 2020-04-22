36f2bac6705f9f00d2ad9dd9aa940ef400306ad7
Yuan-Kuei Hsueh <ph.study@gmail.com>
2020-04-22 10:58:36 +0800

    bump spark to 2.4.5

--- tenmax-apache-spark.rb
+++ tenmax-apache-spark.rb
@@ -1,20 +1,23 @@
 class TenmaxApacheSpark < Formula
   desc "Engine for large-scale data processing"
   homepage "https://spark.apache.org/"
-  url "http://archive.apache.org/dist/spark/spark-2.1.0/spark-2.1.0-bin-hadoop2.7.tgz"
-  version "2.1.0"
-  sha256 "0834c775f38473f67cb122e0ec21074f800ced50c1ff1b9e37e222a0069dc5c7"
+  url "https://www.apache.org/dyn/closer.lua?path=spark/spark-2.4.5/spark-2.4.5-bin-hadoop2.7.tgz"
+  mirror "https://archive.apache.org/dist/spark/spark-2.4.5/spark-2.4.5-bin-hadoop2.7.tgz"
+  version "2.4.5"
+  sha256 "020be52524e4df366eb974d41a6e18fcb6efcaba9a51632169e917c74267dd81"
   head "https://github.com/apache/spark.git"
 
   bottle :unneeded
 
+  depends_on :java => "1.8"
+
   resource "hadoop-azure" do
-    url "http://central.maven.org/maven2/org/apache/hadoop/hadoop-azure/2.7.3/hadoop-azure-2.7.3.jar"
+    url "https://repo1.maven.org/maven2/org/apache/hadoop/hadoop-azure/2.7.3/hadoop-azure-2.7.3.jar"
     sha256 "41ac695ff79e86d89543c85c75125431e4a473c4b564cb3cb05993647ffaa016"
   end
 
   resource "azure-storage" do
-    url "http://central.maven.org/maven2/com/microsoft/azure/azure-storage/2.0.0/azure-storage-2.0.0.jar"
+    url "https://repo1.maven.org/maven2/com/microsoft/azure/azure-storage/2.0.0/azure-storage-2.0.0.jar"
     sha256 "847e8fc49faabfaf6344c002d9c22be0ee72ee21809fa5aec2fc8fcfe332b607"
   end
 
@@ -29,7 +32,8 @@ class TenmaxApacheSpark < Formula
 
     rm_f Dir["bin/*.cmd"]
     libexec.install Dir["*"]
-    bin.write_exec_script Dir["#{libexec}/bin/*"]
+    bin.install Dir[libexec/"bin/*"]
+    bin.env_script_all_files(libexec/"bin", Language::Java.java_home_env("1.8"))
 
     (libexec/"jars").install resource("hadoop-azure")
     (libexec/"jars").install resource("azure-storage")
@@ -39,6 +43,8 @@ class TenmaxApacheSpark < Formula
   end
 
   test do
-    system "#{bin}/spark-shell <<<'sql(\"select 1\").show()'"
+    assert_match "Long = 1000",
+      pipe_output(bin/"spark-shell --conf spark.driver.bindAddress=127.0.0.1",
+                  "sc.parallelize(1 to 1000).count()")
   end
 end
